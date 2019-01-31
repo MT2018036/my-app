48492960d3e8c8b24f1194e07e0ea4ac52d768af
MT2018036 <42197425+MT2018036@users.noreply.github.com>
2019-01-31 13:06:40 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,8 +6,39 @@ package com.mycompany.app;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+    public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter Operation:\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Modulus");
+		int n = Integer.parseInt(br.readLine());
+		int result = 0;
+		int x = 0;
+		int y = 0;
+		System.out.println("Enter First Number");
+		x = Integer.parseInt(br.readLine());
+		System.out.println("Enter Secong Number");
+		y = Integer.parseInt(br.readLine());
+		switch (n)
+		{
+			case 1:
+			result = x + y;
+			break;
+			case 2:
+			result = x - y; //Added By Subtract 
+			break;
+			case 3:
+			result = x * y; //Added by Multiply
+			break;
+			case 4:
+			result = x / y; //Added by Divide
+			case 5:
+			result = x % y; //Added by Mod
+			break;
+			default:
+			System.out.println("Invalid Choice");	
+
+		}
+		System.out.println("Answer:" + result);	
+		//Testing for merge showing up on Github		
+}
 }
